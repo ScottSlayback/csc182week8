d130c0c65debd0ad571d99ab118d4bbcff02297c
ScottSlayback <scott.slayback1973@gmail.com>
2017-03-09 12:21:31 -0500

    2nd commit

--- client/client.js
+++ client/client.js
@@ -1,22 +1,22 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.header.events({
+  'click #demo': function() {
+    console.log("You clicked me!");
   },
+    'submit form': function(event) {
+        event.preventDefault();
+        Messages.insert({
+            user: Meteor.user().emails[0].address,
+            message: event.target.addMessage.value,
+            when: new Date(),
+        });
+        event.target.addMessage.value='';
+    },
 });
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.main.helpers({
+  message: function() {
+      return Messages.find().fetch();
+  }
 });
